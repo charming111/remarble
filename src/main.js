3e6ce216d46fce0435a0c4b77d4396970c7a5bdb
charming111 <115520589+charming111@users.noreply.github.com>
2024-02-03 21:00:36 +0900

    js-home

--- src/main.js
+++ src/main.js
@@ -3,9 +3,18 @@
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
 document.addEventListener('scroll', () => {
-    if (window.scrollY > headerHeight ) {
+    if (window.scrollY > headerHeight) {
         header.classList.add('header--dark');
     } else {
-        header.classList.remover('header--dark');
+        header.classList.remove('header--dark');
     }
+});
+
+// home 섹샨을 아래로 스크롤시 투명하게 처리함
+
+const home = document.querySelector('.home__container');
+const homeHeight = home.offsetHeight;
+document.addEventListener('scroll', ()=> {
+    console.log(1 - window.scrollY / homeHeight);
+    home.style.opacity = 1 - window.scrollY / homeHeight;
 });
